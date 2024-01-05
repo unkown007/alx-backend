cb85dea95bde11e610f7aa39c3c2a87a75f9c952
unkown007 <victormatusse00@gmail.com>
2024-01-05 23:06:38 +0200

    added file

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -35,8 +35,9 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         assert (isinstance(page, int) and isinstance(page_size, int))
         assert (page > 0 and page_size > 0)
-        idx = index_range(page, page_size)
-        self.dataset()
-        if idx[0] > len(self.__dataset):
+        size = len(self.dataset())
+        start, end = index_range(page, page_size)
+        end = min(end, size)
+        if start >= size:
             return []
-        return self.__dataset[idx[0]:idx[1]]
+        return self.dataset()[start:end]
