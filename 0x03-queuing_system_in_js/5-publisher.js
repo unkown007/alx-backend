81c28f8ebc54bb3dcc3e3c0fc03d5d9342e94d78
unkown007 <victormatusse00@gmail.com>
2024-06-14 10:05:36 +0200

    added file

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,25 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function publishMessage(message, time) {
+  await sleep(time);
+  console.log(`About to send ${message}`);
+  client.publish('holberton school channel', message);
+}
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+  publishMessage("Holberton Student #1 starts course", 100);
+  publishMessage("Holberton Student #2 starts course", 200);
+  publishMessage("KILL_SERVER", 300);
+  publishMessage("Holberton Student #3 starts course", 400);
+});
