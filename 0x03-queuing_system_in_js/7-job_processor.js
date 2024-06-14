63af318b46ec46bb601b981b25b9ab439eec8ffb
unkown007 <victormatusse00@gmail.com>
2024-06-14 12:23:15 +0200

    added file

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,33 @@
+import { createQueue } from 'kue';
+
+const BLACKLISTED_NUMBERS = ['4153518780', '4153518781'];
+const queue = createQueue();
+
+function sendNotification(phoneNumber, message, job, done) {
+  const total = 2;
+  let pending = 2;
+
+  const sendInterval = setInterval(() => {
+    if (total - pending <= total / 2) {
+      job.progress(total - pending, total);
+    }
+
+    if (BLACKLISTED_NUMBERS.includes(phoneNumber)) {
+      done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+      clearInterval(sendInterval);
+      return;
+    }
+
+    if (total === pending) {
+      console.log(
+        `Sending notification to ${phoneNumber}, with message: ${message}`,
+      );
+    }
+    --pending || done();
+    pending || clearInterval(sendInterval);
+  }, 1000);
+}
+
+queue.process('push_notification_code_2', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
